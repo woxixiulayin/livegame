cc96e7f3a8f268484f177c8e740eb897c11713d3
Jackson Liu <416883813@qq.com>
2016-08-19 18:44:18 +0800

    添加媒体查询，更改容器宽度

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,6 +1,6 @@
 <template>
   <nav class='container'>
-  <div class='nav-bar'>
+  <div class='content-wrap'>
     <ul class='nav'>
       <li v-for='iterm in categories'>
         {{ iterm.name }}
@@ -26,12 +26,9 @@ export default {
 <style lang='scss'>
 @import '../assets/css/main.scss';
 
-$itemwidth: 5rem;
-$bgcol: rgb(52, 73, 94);
-$height: 3rem;
-$fontsize: 1.25rem;
-$btnactcol: rgb(26, 188, 156);
-$btnhovercol: rgb(47, 226, 191);
-
+.nav-bar {
+  /*width: $content-width;*/
+  margin: 0 auto;
+}
 
 </style>
