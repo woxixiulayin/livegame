a7918150ba3bcfb2c2e705ee8847830e0596343d
Jackson Liu <416883813@qq.com>
2016-08-20 15:20:15 +0800

    nav base design

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,18 +1,23 @@
 <template>
-  <nav class='container'>
+  <nav>
+  <div class="container nav-main">
   <div class='content-wrap'>
-    <ul class='nav-main'>
+    <ul>
       <li v-for='iterm in categories'
-          :class="['nav-item', $index==index? 'active': '']">
-        <a>{{ iterm.name }}</a>
+          :class="$index==index? 'active': ''">
+        <a class='nav-item' href="#">{{ iterm.name }}</a>
       </li>
     </ul>
-    <ul class='nav-sub'>
-      <li v-for='site in categories[index].sites'
-        :class="['nav-item']">
-        <a>{{ site }}</a>
+    </div>
+  </div>
+  <div class="container nav-sub">
+    <div class='content-wrap'>
+    <ul>
+      <li v-for='site in categories[index].sites'>
+        <a class='nav-item' href="#">{{ site }}</a>
       </li>
     </ul>
+  </div>
   </div>
   </nav>
 </template>
@@ -34,30 +39,47 @@ export default {
 @import '../assets/css/main.scss';
 
 $height: 2.8rem;
+$nav-fontsize-base: 1.3rem;
+$font-color-base: rgb(220,220,220);
+$bg-color-base: rgb(35,38,42);
+$main-li-hover: rgb(55,61,65);
+$bg-br-color: #ccc;
+$sub-size-fix: 0.8;
 
-nav.container {
-  background-color: $bar-bg-color;
-}
-
-.nav-main {
-  font-size: 1.1rem;
-  font-weight: bold;
-  height: $height;
-  line-height: $height;
-  color: white;
-}
-
-.nav-sub {
-  font-size: .9rem;
-  height: $height * 0.8;
-  line-height: $height * 0.8;
+nav {
+  background: $bg-color-base;
 }
 
 .nav-item {
-  display: inline-block;
   float: left;
-  padding: 0 1rem;
+}
 
+.nav-main {
+  border-bottom: 1px solid lighten($bg-color-base, 20%);
+}
+
+.nav-main .nav-item {
+  font-size: $nav-fontsize-base;
+  line-height: $height;
+  font-weight: bold;
+  color: $font-color-base;
+  @include font-padding;
+
+  &:hover {
+    background: $main-li-hover;
+  }
+}
+
+.nav-sub .nav-item {
+  font-size: $nav-fontsize-base * $sub-size-fix;
+  line-height: $height * $sub-size-fix;
+  font-weight: bold;
+  color: $font-color-base;
+  @include font-padding;
+
+  &:hover {
+    background: $main-li-hover;
+  }
 }
 
 </style>
